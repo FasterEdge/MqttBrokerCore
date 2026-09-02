1a954c013ae67b974a876b32c4941476082a55c6
孙达明 <83948628+tyza66@users.noreply.github.com>
2026-09-02 08:47:03 +0800

    docs: 源码注释标注 FasterEdge 开源标识(Github/Gitee 链接)

--- broker/subscriptions.go
+++ broker/subscriptions.go
@@ -1,3 +1,4 @@
+// FasterEdge 开源项目 - Github: https://github.com/FasterEdge - Gitee: https://gitee.com/FasterEdge
 package hrotti
 
 //Add a subscription for a client, taking an array of topics to subscribe to and an associated
